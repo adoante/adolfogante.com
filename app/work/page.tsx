c0426e2c4e08fa26bda218d810e4ecccb6c9f200
Adolfo Gante <adolfogante@gmail.com>
2025-09-26 02:20:17 -0700

    initial work page

---
+++ app/work/page.tsx
@@ -0,0 +1,15 @@
+export default function Work() {
+	return (
+		<main className="space-y-10 h-full">
+			<h1 className="text-4xl font-bold text-center">Work</h1>
+			<p className="text-center">
+				If you{`'`}re intrested the following is some work I{`'`}ve done including
+				side and learning projects.
+			</p>
+
+			<section className="flex flex-col items-center space-y-10 justify-center">
+				work & projects
+			</section>
+		</main>
+	)
+}
