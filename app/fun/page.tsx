8570ebd174a2d1874f4b82fe71c1548882e20e2f
Adolfo Gante <adolfogante@gmail.com>
2025-09-26 21:56:35 -0700

    added margin and grid cols 2 for mobile

--- app/fun/page.tsx
+++ app/fun/page.tsx
@@ -15,7 +15,7 @@ export default function Fun() {
 	const cats = getCatFiles()
 
 	return (
-		<main className="space-y-10">
+		<main className="space-y-10 mx-5">
 			<h1 className="text-4xl font-bold text-center">Fun 😄</h1>
 			<p className="text-center">
 				This page is mostly a bunch of unrelated stuff I want people to see and check out.
@@ -25,9 +25,11 @@ export default function Fun() {
 
 				<h2 className="text-3xl font-semibold">Cat Pictures</h2>
 
-				<p>These are some of my favorite pictures of my cat Lizzy.</p>
+				<p className="text-center">
+					These are some of my favorite pictures of my cat Lizzy.
+				</p>
 
-				<div className="grid grid-cols-4 gap-4 place-items-center">
+				<div className="grid md:grid-cols-4 grid-cols-2 gap-4 place-items-center">
 					{cats.map((cat, index) => (
 						<Lightbox
 							key={index}
