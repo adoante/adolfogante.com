b4aec07489d5243bd1f9280ab16fb4c297949a1f
Adolfo Gante <adolfogante@gmail.com>
2025-10-09 00:25:58 -0700

    wrong content type and you don't specifiy it for formData

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -75,7 +75,6 @@ const Contact = () => {
 		const res = await fetch(`${process.env.NEXT_PUBLIC_TICKET_API_URL}/submit`, {
 			method: "POST",
 			headers: {
-				"Content-Type": "application/json",
 				"Authorization": `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
 			},
 			body: formData
